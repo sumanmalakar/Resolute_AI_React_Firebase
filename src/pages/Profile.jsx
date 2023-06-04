1c910c709f37ef5f8b8253650c239525ceaca581
Suman Malakar <malakars041@gmail.com>
2023-06-04 19:40:53 +0530

    sign is complete and start working with profile section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
+import { getAuth } from 'firebase/auth'
+
 
 const Profile = () => {
-  return (
-    <div>profile</div>
-  )
+  const [user, setUser] = useState(null)
+
+
+
+
+  const auth = getAuth()
+  
+  useEffect(()=>{
+    setUser(auth.currentUser);
+
+
+    console.log(auth.currentUser)
+  }, [])
+
+  return user ? <h1>{user.displayName} </h1>:"Not Logged In"
 }
 
 export default Profile
