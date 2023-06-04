76fb84df8679f25db4f801e4b995f3f2f8c99c2f
Suman Malakar <malakars041@gmail.com>
2023-06-04 18:14:44 +0530

    signup complete using name,email and password

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom';
+
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {db} from '../firebase.config'
+
+
+
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -25,6 +31,31 @@ const SignUp = () => {
      }))
   }
 
+  const onSubmit = async (e) =>{
+    e.preventDefault()
+
+    try {
+
+      const auth = getAuth();
+const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+const user = userCredential.user;
+
+updateProfile(auth.currentUser, {
+  displayName: name,
+})
+
+// redirect to home
+navigate('/')
+
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
 
   return (
     <>
@@ -33,7 +64,7 @@ const SignUp = () => {
         <p className="pageHeader">Welcome Back!</p>
       </header>
 
-   <form>
+   <form onSubmit={onSubmit}>
    <input
     type="text"
      className="nameInput"
